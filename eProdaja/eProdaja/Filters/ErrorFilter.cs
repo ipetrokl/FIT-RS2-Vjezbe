7b26fc3d59aeacbd41a2e8b5d7896dc51f4cfb77
ivan.petrovic <ivan.petrovic@protel.net>
2023-07-10 23:46:54 +0200

    V8 mala ispravka

--- eProdaja/eProdaja/Filters/ErrorFilter.cs
+++ eProdaja/eProdaja/Filters/ErrorFilter.cs
@@ -8,25 +8,25 @@ namespace eProdaja.Filters
 {
 	public class ErrorFilter : ExceptionFilterAttribute
 	{
-        //public override void OnException(ExceptionContext context)
-        //{
-        //    if (context.Exception is UserException)
-        //    {
-        //        context.ModelState.AddModelError("USERERROR", context.Exception.Message);
-        //        context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-        //    }
-        //    else
-        //    {
-        //        context.ModelState.AddModelError("ERROR", "Server side error");
-        //        context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-        //    }
-            
+        public override void OnException(ExceptionContext context)
+        {
+            if (context.Exception is UserException)
+            {
+                context.ModelState.AddModelError("USERERROR", context.Exception.Message);
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            }
+            else
+            {
+                context.ModelState.AddModelError("ERROR", "Server side error");
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            }
 
-        //    var list = context.ModelState.Where(x => x.Value.Errors.Count() > 0)
-        //        .ToDictionary(x => x.Key, y => y.Value.Errors.Select(z => z.ErrorMessage));
 
-        //    context.Result = new JsonResult(new {errors = list});
-        //}
+            var list = context.ModelState.Where(x => x.Value.Errors.Count() > 0)
+                .ToDictionary(x => x.Key, y => y.Value.Errors.Select(z => z.ErrorMessage));
+
+            context.Result = new JsonResult(new { errors = list });
+        }
     }
 }
 
